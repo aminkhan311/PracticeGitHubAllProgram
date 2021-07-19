9317ca334263c3858d9a626a2c138809552146a9
amin.khan <aminkhan311@gmail.com>
2021-07-19 18:39:29 +0530

    19/07/2021

---
+++ Operators/arithmetic.py
@@ -0,0 +1,13 @@
+print("addition two numbers:",10+20)
+print("Addition two Str:","amin"+" khan")
+print("Substraction:",20-10)
+print("Multiplication:",10*5)
+print("Str multiplication:","abc"*3)
+print("Division:",100/20)
+print("Modulo Division:",100%3)
+print("Exponent",10**2)
+print("Floor division:",100//3)
+
+
+
+
